2526daa9906af652dea718109e861f73e5e744fb
samuelgsrmgc <hizzx@163.com>
2022-03-30 13:40:43 +0800

    test

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const chalk = require('chalk');
+const conf = require('./config/defaultConfig');
+
+const server = http.creatServer((req, res) =>{
+
+
+});
+
+server.listen(conf.port, conf.hostname, () =>{
+
+});
